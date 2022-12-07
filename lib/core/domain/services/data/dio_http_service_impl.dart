842b72779cf9d3d436eaf27131b9bed117c957d6
Leonardo Serrano <dev.leonardoserrano@gmail.com>
2022-12-07 11:12:29 +0000

    Create const utils.

--- lib/core/domain/services/data/dio_http_service_impl.dart
+++ lib/core/domain/services/data/dio_http_service_impl.dart
@@ -1,12 +1,17 @@
 import 'package:dio/dio.dart';
 
+import '../../../utils/core_const.dart';
 import '../../http_service.dart';
 
 class DioHttpServiceImpl implements HttpService {
   late Dio dio;
 
   DioHttpServiceImpl() {
-    dio = Dio();
+    dio = Dio(
+      BaseOptions(
+        baseUrl: CoreConst.apiBaseUrl.value,
+      ),
+    );
   }
 
   @override
